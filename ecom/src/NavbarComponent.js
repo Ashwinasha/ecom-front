db05206bf9da7bbca2ed52d43ad5864a64a9e25f
Ashwin <ashwinvenu68@gmail.com>
2024-07-11 19:22:22 +0530

    small changes made

--- ecom/src/NavbarComponent.js
+++ ecom/src/NavbarComponent.js
@@ -11,8 +11,7 @@ const NavbarComponent = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- ecom/src/ProductCard.js
+++ ecom/src/ProductCard.js
@@ -1,16 +1,21 @@
 // src/ProductCard.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import queryString from 'query-string';
 
-const ProductCard = ({ image, title, text }) => {
+const ProductCard = ({ image, title, text, price }) => {
+    const productInfo = queryString.stringify({ image, title, text, price });
+
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={image} />
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{text}</Card.Text>
-                <Button variant="primary">Buy Now</Button>
+                <Link to={`/buy-now?${productInfo}`}>
+                    <Button variant="primary">Buy Now</Button>
+                </Link>
             </Card.Body>
         </Card>
     );
